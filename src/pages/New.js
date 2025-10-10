e3f9ad3e1dc69b726e5ca149bb8c0c3ac64e1126
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-10 09:18:28 +0900

    새 일기 쓰기 Header

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../component/Button";
+import Header from "../component/Header";
+
 const New = () => {
+
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    };
+
     return (
         <div>
-            <h2>New 페이지 입니다</h2>
+            <Header title={"새 일기 쓰기"} 
+                    leftChild={<Button text={"< 뒤로 가기"} onClick={goBack}/>}
+            />
         </div>
     )
 }
